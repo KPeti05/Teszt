ca9d815df352d78d7700e42e6f7a9137ccc772d4
KPeti05 <izompeti@gmail.com>
2024-09-25 10:46:50 +0200

    Szöveg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niggers!");
+            Console.WriteLine("wasd!");
             Console.ReadKey();
         }
     }
